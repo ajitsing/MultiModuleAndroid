14aa5d24ebce69343279a6b7e5a334b8d1452bef
AjitSingh <jeetsingh.ajit@gmail.com>
2016-07-31 15:43:24 +0530

    breaking commit

--- mylibrary/src/main/java/com/ajit/singh/mylibrary/ViewModel.java
+++ mylibrary/src/main/java/com/ajit/singh/mylibrary/ViewModel.java
@@ -1,13 +1,23 @@
 package com.ajit.singh.mylibrary;
 
-public class ViewModel {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import com.ajit.singh.mylibrary.BR;
+
+public class ViewModel extends BaseObservable {
   private String name;
 
   public ViewModel(String name) {
     this.name = name;
   }
 
+  @Bindable
   public String getName() {
     return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+    notifyPropertyChanged(BR.name);
   }
 }
