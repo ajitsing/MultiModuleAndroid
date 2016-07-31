e72b8d9fafed1564ed0a908cd78c4cec826fad32
AjitSingh <jeetsingh.ajit@gmail.com>
2016-07-31 10:27:07 +0530

    using data binding

--- mylibrary/src/main/java/com/ajit/singh/mylibrary/MyClass.java
+++ mylibrary/src/main/java/com/ajit/singh/mylibrary/MyClass.java
@@ -1,13 +1,17 @@
 package com.ajit.singh.mylibrary;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+
+import com.ajit.singh.mylibrary.databinding.MyBinding;
 
 public class MyClass extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.mylayout);
+    MyBinding binding = DataBindingUtil.setContentView(this, R.layout.mylayout);
+    binding.setViewModel(new ViewModel("Ajit"));
   }
 }
---
+++ mylibrary/src/main/java/com/ajit/singh/mylibrary/ViewModel.java
@@ -0,0 +1,13 @@
+package com.ajit.singh.mylibrary;
+
+public class ViewModel {
+  private String name;
+
+  public ViewModel(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
